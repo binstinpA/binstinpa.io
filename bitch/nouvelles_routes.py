18c34873776047de024ac24a5ba1e3c3a68c8e46
binstinpA <3ddr3sur4@gmail.com>
2024-05-30 20:50:15 +0200

    Add files via upload

---
+++ bitch/nouvelles_routes.py
@@ -0,0 +1,8 @@
+﻿from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///collections.db'
+db = SQLAlchemy(app)
+
+# Vos autres routes et configurations pour la base de données...
---
+++ bitch/stripe_config.py.py
@@ -0,0 +1,7 @@
+﻿# stripe_config.py
+
+import stripe
+import os
+
+# Votre clé secrète Stripe
+stripe.api_key = os.getenv('STRIPE_SECRET_KEY', 'sk_test_...')
