6f1763bb9fab67d74c6591485718b844abe1b9a4
binstinpA <3ddr3sur4@gmail.com>
2024-05-31 11:42:28 +0200

    Delete bitch directory

--- bitch/nouvelles_routes.py
+++
@@ -1,8 +0,0 @@
-﻿from flask import Flask, render_template, request, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///collections.db'
-db = SQLAlchemy(app)
-
-# Vos autres routes et configurations pour la base de données...
